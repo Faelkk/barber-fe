2c12f06809f78a2cd2af21b8524866ba55d0b0bc
faelkkk <Achten.rafael16@gmail.com>
2025-02-04 11:30:53 -0300

    feat: add component unidade empty

--- src/app/components/pages/unidades/components/unidades-container/Unidades-container.tsx
+++ src/app/components/pages/unidades/components/unidades-container/Unidades-container.tsx
@@ -1,10 +1,11 @@
 import getUnits from "@/actions/units/get-units";
 import UnidadesSection from "../../unidades-section/Unidades-section";
+import UnidadesEmpty from "../unidades-empty/Unidades-empty";
 
 export default async function UnidadesContainer() {
   const { data, error, ok } = await getUnits();
 
-  if (error) return <h2>Um erro ocorreu.</h2>;
+  if (error) return <UnidadesEmpty />;
 
   if (data && ok) return <UnidadesSection units={data} />;
 }
---
+++ src/app/components/pages/unidades/components/unidades-empty/Unidades-empty.tsx
@@ -0,0 +1,33 @@
+import Button from "@/app/components/ui/button/Button";
+import Container from "@/app/components/ui/container/Container";
+
+export default function UnidadesEmpty() {
+  return (
+    <main className="flex flex-col justify-center items-center mt-[120px]">
+      <Container>
+        <header className="flex items-center pp:gap-3 flex-row w-[90%] justify-between">
+          <h1 className="font-merriweather font-bold text-lg mini:text-2xl pp:text-3xl md:text-4xl text-black pp:relative before:content-[''] before:bg-black pp:ml-[30px] before:w-4 before:h-4 before:absolute before:left-[-30px] before:top-1/2 before:-translate-y-1/2">
+            Nossas unidades
+          </h1>
+        </header>
+      </Container>
+
+      <Container>
+        <div className="flex  items-center pp:gap-3 flex-row w-[90%] justify-between">
+          <span className="font-poppins text-xl mt-2">
+            Nenhuma unidade foi encontrada no momento.
+          </span>
+        </div>
+      </Container>
+      <Button
+        href="/agendar-horario"
+        borderColor="border-cold-gray-900"
+        textColor="text-cold-gray-900"
+        className="mt-16 flex justify-center"
+        ariaLabel="Agende seu horario para atendimento"
+      >
+        Agendar horario
+      </Button>
+    </main>
+  );
+}
